d2594dba32e2afc107dd1090a986f8e071a34997
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2024-10-06 21:07:22 +0100

    Added drizzle setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-const mockUrls = [
+const mockUrls: string[] = [
   "https://utfs.io/f/fWZ41OxcUGsED096JD4WVYX1aheQpvzlgdJW9bSKRyOUuxwN",
   "https://utfs.io/f/fWZ41OxcUGsEvonuQedmufUXrgz0s41yPeG5dxElC38h2bqa",
   "https://utfs.io/f/fWZ41OxcUGsEDBOtHSWVYX1aheQpvzlgdJW9bSKRyOUuxwNo",
@@ -11,10 +12,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="py-4">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <p key={post.id} className="w-48">
+            {post.name}
+          </p>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="" className="w-full" />
